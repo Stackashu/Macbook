3c1a2b396e4f5d0138e7d0c8d01ecfd3cb8e022a
Stackashu <ashu12141214@gmail.com>
2025-01-27 16:22:15 +0530

    fix code

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Update this if hosting under a subpath
+ 
+  base: "/", // Update this if hosting under a subpath
   build: {
-    outDir: 'dist', // Default output directory
+    outDir: "dist", // Default output directory
+    chunkSizeWarningLimit: 1000,
   },
-})
+});
